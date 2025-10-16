2a291c6d90bc9f48bee174f8921f5e9eefe516b5
Gautam-Sarraf <122592561+Gautam-Sarraf@users.noreply.github.com>
2025-10-16 20:33:06 +0530

    change

--- app/page.js
+++ app/page.js
@@ -1,8 +1,4 @@
-import Image from "next/image";
-import styles from "./page.module.css";
 import "./globals.css";
-import Header from "./components/header/header";
-import HeroSection from "./components/hero/hero";
 import HeaderHeroWrapper from "./components/bachground/background";
 import TherapyAccess from "./components/therapyAccess/therapyAccess";
 import WelcomeSection from "./components/WelcomeSection/WelcomeSection";
@@ -10,7 +6,7 @@ import ServicesSection from "./components/Services/Services";
 import CareSection from "./components/CareSection/CareSection";
 import BlogSection from "./components/BlogSection/BlogSection";
 import Testimonial from "./components/Testimonial/testimonial";
-import Footer from "./components/Footer/footer";
+
 export default function Home() {
   return (
     <>
