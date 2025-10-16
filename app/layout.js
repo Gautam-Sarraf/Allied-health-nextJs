f873113938acee1e0d19fe0a0efa15c32a75e5c9
Gautam-Sarraf <122592561+Gautam-Sarraf@users.noreply.github.com>
2025-10-16 20:37:27 +0530

    footer

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "./components/header/header";
-import Footer from "./components/footer/footer";
+import Footer from "./components/Footer/footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
